7ad27b250fe45b94c8796103494f9d165e10118d
Harland <lohoraharland@gmail.com>
2018-05-13 21:09:46 -0500

    First Function

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
 
-function getTriangle(){
-
+function calculate(){
+	var b = parseFloat(document.getElementById("base").value);
+	alert(b);
+	alert(typeof(b));
 }
