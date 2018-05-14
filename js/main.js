58491f722f24e8d988febecd434e5c04caf1d078
Harland <lohoraharland@gmail.com>
2018-05-13 21:40:54 -0500

    Display Area

--- js/main.js
+++ js/main.js
@@ -3,7 +3,10 @@ var ctx = canvas.getContext("2d");
 
 
 function calculate(){
-	var b = parseFloat(document.getElementById("base").value);
-	alert(b);
-	alert(typeof(b));
+	var b      = parseFloat(document.getElementById("base").value);
+	var h      = parseFloat(document.getElementById("height").value);
+	var result = (b*h)/2;
+	document.getElementById("none").style.display = "initial";
+	document.getElementById("result").innerHTML = result;
+
 }
